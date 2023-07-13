4c20b89e5eebaa2aa9ef06fba47910497a3d0546
Momsen Reincke <35260345+momsenr@users.noreply.github.com>
2023-07-13 12:53:15 -0700

    change to pandas instead of dict to enable hdf5 loading

--- cancerrisknet/datasets/filter.py
+++ cancerrisknet/datasets/filter.py
@@ -1,37 +1,35 @@
 from cancerrisknet.utils.date import parse_date
 
-#MIN_FOLLOWUP_YEAR_IF_NEG = 10  # TODO: this should move to data/SETTINGS or parser too.
-
-
 def get_avai_trajectory_indices(patient, events, args):
     """
-        This function takes a patients and its events, age and gender information
-        and returns all the valid trajectories indexes depending on the filters applied.
+        This function takes a patients and its events as pandas dataframe and returns all rows that are valid
+        trajectories depending on the filters applied.
 
         Filters implemented in this version:
-            - Remove patients that are not of the given split group
             - Exclusion interval: Removes events too close to PC. If exclusion interval is 0 then do not remove. 
 
         Returns:
-            valid_indices (list of int): Each index is corresponding to one partial trajectory that passed the filter.
+            valid_trajectories (pandas df): Each row is corresponding to one partial trajectory that passed the filter.
             y (bool): If valid_indices is not empty, then y indicates whether any of the trajectories
                       include a cancer diagnosis.
 
     """
-    valid_indices = []
     y = False
 
-    for idx in range(len(events)):
+    #add column is_valid_idx to df
+    events['is_valid_idx'] = False
+
+    for idx, row in events.iterrows():
         if patient['future_panc_cancer'] and \
-                (patient['outcome_date'] - events[idx]['admit_date']).days <= 30 * args.exclusion_interval:
+                (patient['outcome_date'] - row['admit_date']).days <= 30 * args.exclusion_interval:
             continue
 
-        if is_valid_trajectory(events[:idx+1], patient['outcome_date'], patient['future_panc_cancer'], args):
-            valid_indices.append(idx)
-            days_to_censor = (patient['outcome_date']-events[idx]['admit_date']).days
+        if is_valid_trajectory(events.iloc[:idx+1], patient['outcome_date'], patient['future_panc_cancer'], args):
+            row['is_valid_idx'] = True
+            days_to_censor = (patient['outcome_date'] - row['admit_date']).days
             y = (days_to_censor < (max(args.month_endpoints) * 30) and patient['future_panc_cancer']) or y
 
-    return valid_indices, y
+    return events, y
 
 
 def is_valid_trajectory(events_to_date, outcome_date, future_panc_cancer, args):
@@ -55,13 +53,12 @@ def is_valid_trajectory(events_to_date, outcome_date, future_panc_cancer, args):
         return False
 
     # Filter (2-3)
-    is_pos_pre_cancer = events_to_date[-1]['admit_date'] < outcome_date
-    is_pos_in_time_horizon = (outcome_date - events_to_date[-1]['admit_date']).days < max(args.month_endpoints) * 30
+    is_pos_pre_cancer = events_to_date.iloc[-1]['admit_date'] < outcome_date
+    is_pos_in_time_horizon = (outcome_date - events_to_date.iloc[-1]['admit_date']).days < max(args.month_endpoints) * 30
     is_valid_pos = future_panc_cancer and is_pos_pre_cancer and is_pos_in_time_horizon
 
     # Filter (4)
     is_valid_neg = not future_panc_cancer and \
-        (outcome_date - events_to_date[-1]['admit_date']).days // 365 > args.min_followup_year_if_neg
+        (outcome_date - events_to_date.iloc[-1]['admit_date']).days // 365 > args.min_followup_year_if_neg
 
     return is_valid_neg or is_valid_pos
-
