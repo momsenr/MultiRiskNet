e0598a03c0c68693335124f988820bc9512e291e
mreincke <momsen.reincke@charite.de>
2023-09-19 19:33:04 -0700

    implemented multitask-loss

--- cancerrisknet/learn/step.py
+++ cancerrisknet/learn/step.py
@@ -1,53 +1,79 @@
 import torch
 import torch.nn.functional as F
 
+def get_multi_task_loss(logits, batch, args, task_weights=None):
+    """
+    Compute multi-task loss.
 
-def get_model_loss(logit, batch, args):
+    Args:
+        logits (torch.Tensor): Predicted logits for all tasks. Shape: [num_tasks, *]
+        batch (dict): Batch containing labels and masks for all tasks.
+        args: Arguments containing loss function choice.
+        task_weights (list or torch.Tensor, optional): Weights for each task. If not provided,
+                                                       all tasks are treated equally.
+
+    Returns:
+        torch.Tensor: Total multi-task loss.
+    """
+
     y_seq = batch['y_seq']
     y_mask = batch['y_mask']
+
+    # If no specific task weights are provided, assume equal weights for all tasks.
+    if task_weights is None:
+        task_weights = torch.ones(logits.shape[0]).to(logits.device)
+    else:
+        task_weights = torch.tensor(task_weights).to(logits.device)
+
     if args.loss_fn == 'binary_cross_entropy_with_logits':
-        loss = F.binary_cross_entropy_with_logits(logit, y_seq.float(), weight=y_mask.float(), reduction='sum')\
-               / torch.sum(y_mask.float())
+        # Compute BCE loss for all tasks
+        losses = F.binary_cross_entropy_with_logits(logits, y_seq, weight=y_mask, reduction='none')
+        # Sum over the sequence dimension and then divide by the sum of the masks for each task
+        losses = torch.sum(losses, dim=-1) / torch.sum(y_mask, dim=-1)
     elif args.loss_fn == 'mse':
-        loss = F.mse_loss(logit, y_seq.float(), size_average=False, reduction='mean')
+        # Compute MSE loss for all tasks, adjust to sum the losses and then average over tasks
+        losses = F.mse_loss(logits, y_seq, reduction='sum').div(logits.shape[0])
     else:
         raise Exception('Loss function is illegal or not found.')
-    return loss
 
+    # Weighted sum of all task losses
+    total_loss = torch.sum(task_weights * losses)
 
-def model_step(batch, models, train_model, args):
+    return total_loss
+
+def model_step(batch, models, train_model, args, task_weights=None):
     """
-        Single step of running model on the a batch x,y and computing the loss.
-        Returns various stats of this single forward and backward pass.
+    Single step of running model on a batch x,y for multi-task learning and computing the loss.
+    Returns various stats of this single forward and backward pass.
 
-        Args:
-            batch: whole batch dict, can be used by various special args
-            models: dict of models. The main model, named "model" must return logit, hidden, activ
-            train_model: Backward pass is computed if set to True.
+    Args:
+        batch: whole batch dict, can be used by various special args
+        models: dict of models. The main model, named "model" must return logit, hidden, activ for each task.
+        train_model: Backward pass is computed if set to True.
 
-        Returns:
-            loss: scalar for loss on batch as a tensor
-            preds: predicted labels as numpy array
-            probs: softmax probablities as numpy array
-            golds: labels at the trajectory level, numpy array version of arg y
-            patient_golds: labels at the patient level
-            exams: exam ids for batch if available as a list of strings
-            pids: deidentified patient ids as a list of strings
-            censor_times: feature rep for batch
-            days_to_censor: the time before censorship as a tensor
-            dates: the admission data as a tensor
+    Returns:
+        loss: scalar for loss on batch as a tensor
+        preds: predicted labels as numpy array
+        probs: softmax probabilities as numpy array
+        golds: labels at the trajectory level, numpy array version of arg y
+        patient_golds: labels at the patient level for each task
+        pids: deidentified patient ids as a list of strings
+        censor_times: feature rep for batch
+        days_to_censor: the time before censorship as a tensor
+        dates: the admission date as a tensor
     """
-    logit = models[args.model_name](batch['x'], batch)
-    loss = get_model_loss(logit, batch, args)
+    logits = models[args.model_name](batch['x'], batch)
+    loss = get_multi_task_loss(logits, batch, args,task_weights=task_weights)
 
     if train_model:
         loss.backward()
 
-    probs = torch.sigmoid(logit).cpu().data.numpy()  # Shape is B, len(args.month_endpoints)
+    # Use sigmoid for multi-label tasks and convert to numpy
+    probs = torch.sigmoid(
+        logits).cpu().data.numpy()  # Shape is T, B, len(args.month_endpoints) where T is number of tasks
     preds = probs > .5
-    golds = batch['y'].data.cpu().numpy()
-    patient_golds = batch['future_panc_cancer'].data.cpu().numpy()
-    #exams = batch['exam']
+    golds = batch['y'].data.cpu().numpy()  # This is a 2D tensor with tasks as the first dimension
+    patient_golds = batch['future_panc_cancer'].data.cpu().numpy() # This is a 2D tensor with tasks as the first dimension
     pids = batch['patient_id'].cpu().numpy()
     censor_times = batch['time_at_event'].cpu().numpy()
     days_to_censor = batch['days_to_censor'].cpu().numpy()
