757b6b45ed9055c9af1b5c3a8d3d3b352255accb
Momsen Reincke <mreincke@anes-nalab5>
2023-10-26 16:34:44 -0700

    take no_random_sample_eval_trajectories from config file when resuming

--- cancerrisknet/utils/parsing.py
+++ cancerrisknet/utils/parsing.py
@@ -178,7 +178,7 @@ def parse_args(args_str=None):
                         '--dataset', '--num_workers',
                         '--eval_batch_size', '--max_batches_per_dev_epoch', 
                         '--resume_from_result']
-        keep_args_from_config = ['train', 'dev', 'test', 'attribute','metadata_path', 'save_dir', 'max_eval_indices', 'results_path', 'exp_id']
+        keep_args_from_config = ['train', 'dev', 'test', 'attribute','metadata_path', 'save_dir', 'no_random_sample_eval_trajectories', 'max_eval_indices', 'jeresults_path', 'exp_id']
         for a in overwrite_args:
             if a in sys.argv:  # if specified differently for continued experiments
                 keep_args_from_config.append(a.replace('--',''))
