f4865e0b2b797c67debc84851b1371e962e87b3f
Momsen Reincke <mreincke@anes-nalab5>
2023-10-30 22:16:22 -0700

    add option for uncertainty weights

--- cancerrisknet/utils/parsing.py
+++ cancerrisknet/utils/parsing.py
@@ -100,6 +100,7 @@ def parse_args(args_str=None):
                              'dataloader. Useful when the whole data is too large. Default: False.')
     parser.add_argument('--enforce_strict_monotonicity', action='store_true', default=False)
     parser.add_argument('--init_lr', type=float, default=0.001, help='The initial learning rate [default: 0.001]')
+    parser.add_argument('--use_uncertainty_loss_weights', action='store_true', default=False)
     parser.add_argument('--lr_decay', type=float, default=1., help='Decay of learning rate [default: no decay (1.)]')
     parser.add_argument('--momentum', type=float, default=0, help='Momentum to use with SGD')
     parser.add_argument('--weight_decay', type=float, default=0, help='L2 Regularization penaty [default: 0]')
