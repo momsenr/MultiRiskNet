00bcad3f5e707a2fe84b26a39e62eaa3f0682089
Momsen Reincke <mreincke@stanford.edu>
2025-01-26 19:05:12 +0000

    fix bug when ATTRIBUTION_SUM_METHOD was ignored due to incorrect parsing

--- cancerrisknet/utils/parsing.py
+++ cancerrisknet/utils/parsing.py
@@ -208,7 +208,7 @@ def parse_args(args_str=None):
                         '--save_dir', '--exp_id', '--results_path', '--resume_from_result']
         keep_args_from_config = ['train', 'dev', 'epochs', 'tuning_metric', 'test', 'attribute', \
         'metadata_path', 'no_random_sample_eval_trajectories', 'max_eval_indices', 'cancer_code_dict_path',\
-        'exp_id', 'results_path', 'attribution_method', 'attribution_prediction_threshold', 'loss_weights']
+        'exp_id', 'results_path', 'attribution_method', 'attribution_sum_method', 'attribution_prediction_threshold', 'loss_weights']
         for a in overwrite_args:
             if a in sys.argv:  # if specified differently for continued experiments
                 keep_args_from_config.append(a.replace('--',''))
